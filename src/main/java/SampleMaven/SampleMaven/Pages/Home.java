516306881b8111b210b307985ab57a63cb18590e
deepalitarange <deepalitarange@gmail.com>
2019-04-14 12:56:48 +0530

    new code commit in training

--- src/main/java/SampleMaven/SampleMaven/Pages/Home.java
+++ src/main/java/SampleMaven/SampleMaven/Pages/Home.java
@@ -1,6 +1,7 @@
 package SampleMaven.SampleMaven.Pages;
 
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -10,31 +11,31 @@ import org.openqa.selenium.support.ui.Select;
 import SampleMaven.SampleMaven.SeleniumPractice.testSuite.BaseTest;
 
 public class Home extends BaseTest {
-	
+
 	public static final String btnReturn="//*[@id='Return']";
 	public static final String btnOneWay="//*[@id='oneWay_trigger']";
 	public static final String txtReturn="//*[@id='txtEndDate']";
-	
+
 	public static final String ddlSuffix="//*[@id='TG_Register.G_PIName.Q_Suffix.Suffix']";
-	
-	
+
+
 	public Boolean isReturnSelected() {
 		return driver.findElement(By.xpath(btnReturn)).isSelected();
 	}
-	
+
 	public Boolean isTxtReturnenabled() {
 		return driver.findElement(By.xpath(txtReturn)).isEnabled();
-		
+
 	}
-	
+
 	public void selectsuffix(String suffix) {
 		Select ss= new Select(driver.findElement(By.xpath(ddlSuffix)));
 		ss.selectByVisibleText(suffix);
 		List<WebElement>ll=ss.getOptions();
 		//ss.isMultiple()
-		
+
 		for(WebElement ele:ll) {
-			
+
 			if(ele.getText().equals("India")) {
 				ele.click();
 				break;
@@ -44,24 +45,44 @@ public class Home extends BaseTest {
 		for(int i=0;i<ll.size();i++) {
 			System.out.println(ll.get(i).getText());
 		}
-		
+
 	}
-	
+	/*
+	 * Switch frame code for learning
+	 * 
+	 */
 	public void learningMethods() {
 		Alert aa=driver.switchTo().alert();
 		aa.accept();
 		aa.dismiss();
-		
-	List<WebElement>frames=	driver.findElements(By.tagName("//a"));
-	
+
+		List<WebElement>frames=	driver.findElements(By.tagName("iframe"));
+
 		for(WebElement ele:frames) {
-			
-			
+
+
 			driver.switchTo().frame(ele.getText());
-			
+
 		}
 		driver.switchTo().defaultContent();
-		
+
 	}
 
+	/*
+	 * windows switch code
+	 */
+	public void LearningWindows() {
+		String parentWindow=driver.getWindowHandle();
+		//operation performed like click button
+		Set<String>windows=driver.getWindowHandles();
+		for(String ss:windows) {
+			if(!ss.equalsIgnoreCase(parentWindow)) {
+				driver.switchTo().window(ss);
+				//perform operation here for that window
+				driver.close();
+			}
+
+		}
+		driver.switchTo().window(parentWindow);
+	}
 }
--- src/test/java/SampleMaven/SampleMaven/SeleniumPractice/testSuite/AmazonTest.java
+++ src/test/java/SampleMaven/SampleMaven/SeleniumPractice/testSuite/AmazonTest.java
@@ -1,9 +1,16 @@
 package SampleMaven.SampleMaven.SeleniumPractice.testSuite;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.testng.Assert;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import SampleMaven.SampleMaven.PropertyLoader;
 import SampleMaven.SampleMaven.Pages.Home;
 import SampleMaven.SampleMaven.Pages.Login;
 
@@ -36,15 +43,36 @@ public class AmazonTest extends BaseTest {
 
 	}
 	
-	@Test
-	public void verifyLogin() {
-		
+	@Test(dataProvider="getTestData")
+	public void verifyLogin(String clientId) {
+		System.out.println(clientId);
 		login.loginIntoApplication("https://www.jetairways.com/EN/IN/home.aspx").verifyWelcome();
 		Assert.assertTrue(home.isReturnSelected(),"By default two way is not selected");
 		Assert.assertTrue(home.isTxtReturnenabled(),"return field is disbaled");
 		
 	}
 	
-	
+	@DataProvider
+	private Iterator<Object[]> getTestData() {
+		List<Object[]> profileContent = new ArrayList<Object[]>();
+		String testData;
+		try {
+			testData = new PropertyLoader().getKey("clientId");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			testData=null;
+			e.printStackTrace();
+		}
+		if(testData.contains(",")) {
+			String[] testArray=testData.split(",");
+			for(int i = 0; i < testArray.length; i++) {
+				profileContent.add(new String[] { testArray[i] });
+			}
+		}
+		else {
+			profileContent.add(new String[] {testData});
+		}
+		return profileContent.iterator();
+	}
 
 }
